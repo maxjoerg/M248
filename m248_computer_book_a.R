1491856d7752556e8827eaa3c758a9fb68520141
maxjoerg <max.schleiwies@googlemail.com>
2020-10-10 14:38:36 +0200

    Computer book A, barcharts for tattoos and workforce. Converted wide to long formatfor grouping

--- m248_computer_book_a.R
+++ m248_computer_book_a.R
@@ -24,5 +24,22 @@ colnames(workforce) <- c("Occupation", "Male", "Female", "Total")
 str(workforce)
 
 ggplot(data = workforce, aes(x = reorder(Occupation, -Total), y = Total) ) +
-  geom_bar ( stat = "identity")
+  geom_bar ( stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+#########
+
+library(tidyr)
+workforce_long <- gather(workforce, gender, count, Male:Female, factor_key=TRUE)
+str(workforce_long) 
+
+ggplot(data = workforce_long, aes(x = reorder(Occupation, -Total), y = count, fill = gender) ) +
+  geom_bar ( stat = "identity") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+
+
+
   
